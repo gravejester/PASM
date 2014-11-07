73adf04961e71340606a905241bf1a60c8439a20
Øyvind Kallstad <kallstad@gmail.com>
2014-11-07 17:26:12 +0100

    More stuff & updated readme

--- PASM.psm1
+++ PASM.psm1
@@ -797,7 +797,7 @@ function Get-SorensenDiceCoefficient {
         .DESCRIPTION
             The Sørensen–Dice index, is a statistic used for comparing the similarity of two samples.
         .EXAMPLE
-            Get-SorensenDiceCoefficient -Source 'night' -Target 'night'
+            Get-SorensenDiceCoefficient -String1 'night' -String2 'night'
         .LINK
             http://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
         .NOTES
@@ -839,7 +839,7 @@ function Get-SorensenDiceDistance {
         .DESCRIPTION
             Uses the Sorensen Dice Coefficient to calculate the distance between two strings.
         .EXAMPLE
-            Get-SorensenDiceDistance -Source 'night' -Target 'nacht'
+            Get-SorensenDiceDistance -String1 'night' -String2 'nacht'
         .NOTES
             Author: Øyvind Kallstad
             Date: 03.11.2014
@@ -996,5 +996,83 @@ function Get-JaroWinklerDistance {
 
     else {
         Write-Output $output
+    }
+}
+
+function Get-LevenshteinDistance {
+    <#
+        .SYNOPSIS
+            Get the Levenshtein distance between two strings.
+        .DESCRIPTION
+            The Levenshtein Distance is a way of quantifying how dissimilar two strings (e.g., words) are to one another by counting the minimum number of operations required to transform one string into the other.
+        .EXAMPLE
+            Get-LevenshteinDistance -Source 'kitten' -Target 'sitting'
+        .LINK
+            http://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#C.23
+        .LINK
+            https://github.com/gravejester/PASM
+        .LINK
+            http://en.wikipedia.org/wiki/Edit_distance
+        .NOTES
+            Author: Øyvind Kallstad
+            Date: 07.11.2014
+            Version: 1.0
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Position = 0)]
+        [string]$String1, 
+
+        [Parameter(Position = 1)]
+        [string]$String2,
+
+        # Makes matches case-sensitive. By default, matches are not case-sensitive.
+        [Parameter()]
+        [switch] $CaseSensitive,
+
+        # A normalized output will fall in the range 0 (perfect match) to 1 (no match).
+        [Parameter()]
+        [switch] $NormalizeOutput
+    )
+
+    if (-not($CaseSensitive)) {
+        $String1 = $String1.ToLowerInvariant()
+        $String2 = $String2.ToLowerInvariant()
+    }
+ 
+    $d = New-Object 'Int[,]' ($String1.Length + 1), ($String2.Length + 1)
+        
+    try {
+        for ($i = 0; $i -le $d.GetUpperBound(0); $i++) {
+            $d[$i,0] = $i
+        }
+ 
+        for ($i = 0; $i -le $d.GetUpperBound(1); $i++) {
+            $d[0,$i] = $i
+        }
+ 
+        for ($i = 1; $i -le $d.GetUpperBound(0); $i++) {
+            for ($j = 1; $j -le $d.GetUpperBound(1); $j++) {
+                $cost = [Convert]::ToInt32((-not($String1[$i-1] -ceq $String2[$j-1])))
+                $min1 = $d[($i-1),$j] + 1
+                $min2 = $d[$i,($j-1)] + 1
+                $min3 = $d[($i-1),($j-1)] + $cost
+                $d[$i,$j] = [Math]::Min([Math]::Min($min1,$min2),$min3)
+            }
+        }
+
+        $distance = ($d[$d.GetUpperBound(0),$d.GetUpperBound(1)])
+ 
+        if ($NormalizeOutput) {
+            Write-Output (1 - ($distance) / ([Math]::Max($String1.Length,$String2.Length)))
+        }
+
+        else {
+            Write-Output $distance
+        }   
+    }
+ 
+    catch {
+        Write-Warning $_.Exception.Message
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,25 @@ PASM
 ====
 
 PowerShell Approximate String Matching (PASM)
+
+Algorithms included:
+- Overlap Coefficient
+- Longest Common Substring
+- Longest Common Subsequence
+- Soundex
+- Hamming Distance (modified to work with strings of different lenghts, supports normalized output)
+- Ratcliff/Obershelp Similarity
+- Jaccard Distance & Jaccard Index
+- Sorensen Dice Distance & Sorensen Dice Coefficient
+- Jaro-Winkler Distance (can also output Jaro Distance, supports normalized output)
+- Levenshtein Distance (supports normalized output)
+
+Helper functions:
+- Get-Intersection
+- Get-StringIntersection
+- Get-Union
+- Get-StringUnion
+- Get-NGrams
+- Get-CommonPrefix
+- Get-CommonSuffix
+- ConvertTo-SoundexNumber
